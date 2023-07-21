d17ee18497f681d75c6f438c1b3a7810e05baddd
Chen Yu <yu.c.chen@intel.com>
2023-07-21 00:30:22 -0700

    Add sleep parameter for stress nanosleep
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- stress_nanosleep.c
+++ stress_nanosleep.c
@@ -2,15 +2,22 @@
 // use of nanosleep
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	int response;
+	int response, sleep_ns = 1000;
 	struct timespec remaining, request = { 0, 100 };
+
+	if (argc != 2) {
+		printf("please specify the sleep nanosleep\n");
+		return -1;
+	}
+	sleep_ns = atoi(argv[1]);
 
 	while (1) {
 		request.tv_sec = 0;
-		request.tv_nsec = 100;
+		request.tv_nsec = sleep_ns;
 		response = nanosleep(&request, &remaining);
 	}
 	return 0;

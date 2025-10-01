d415b33495b9be424c987dbd31a5ff8c31e9e1be
Chen Yu <yu.c.chen@intel.com>
2025-10-01 15:59:44 +0800

    Add mmtest scripts
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- sched_scripts/schbench_new_parser/start_parse.sh
+++ sched_scripts/schbench_new_parser/start_parse.sh
@@ -1,18 +1,18 @@
 
 #replace with your directory provided by mmtests
-RESULT_DIR_BASE="spr0/baseline-schbench/"
-RESULT_DIR_CHANGE="spr0/sched_cache-schbench/"
+RESULT_DIR_BASE="amd/baseline-sb/"
+RESULT_DIR_CHANGE="amd/sc-sb/"
 
 rm -rf *.log
 
-for i in 0 1 2 3 4; do
-	for t in 1 2 4 8 16 32 64 128 239; do
+for i in 0; do
+	for t in 1 2 4 8 16 32 64 127; do
 		python3 ./extract_schbench.py ${RESULT_DIR_BASE}"iter-"$i"/schbench/logs/schbench-"$t".log" baseline_total_thread$t.log
 		python3 ./extract_schbench.py ${RESULT_DIR_CHANGE}"iter-"$i"/schbench/logs/schbench-"$t".log" change_total_thread$t.log
 	done
 done
 
-for t in 1 2 4 8 16 32 64 128 239; do
+for t in 1 2 4 8 16 32 64 127; do
 	python3 ./calc_average.py baseline_total_thread$t.log baseline_average_thread$t.log 
 	python3 ./calc_average.py change_total_thread$t.log change_average_thread$t.log 
 	python3 ./check_compare.py baseline_average_thread$t.log change_average_thread$t.log > compare_thread$t.log
